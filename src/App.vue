a8ecf0e9e694237adc411367f0abd08521285e40
ifs20020-itdel <ifs20020@students.del.ac.id>
2022-10-25 02:56:34 +0700

    DOM js for Input and Ouput

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
+
   <AppHeader />
 
   <router-view />
 
   <AppFooter />
+
 </template>
 
 <script>
 import AppFooter from "./components/AppFooter.vue";
 import AppHeader from "./components/AppHeader.vue";
+
 
 export default {
   name: "App",
