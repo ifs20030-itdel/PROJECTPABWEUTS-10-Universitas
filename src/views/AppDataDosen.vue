8249141abaf392f54d1097b4eb6eacaf7e2d2e34
ifs20020-itdel <ifs20020@students.del.ac.id>
2022-10-24 23:43:33 +0700

    Updated

--- src/views/AppDataDosen.vue
+++ src/views/AppDataDosen.vue
@@ -1,193 +1,193 @@
 <template>
- <div class="pseudoheader p-5 mb-2"></div>
-  <div class="main-panel m-5 p-5">
-          <div class="content-wrapper">
-            <div class="row">
-              <div class="col-lg-12 grid-margin stretch-card">
-                <div class="card shadow rounded">
-                  <div class="card-body">
-                    <h4 class="card-title text-center">Daftar Dosen Institut Teknologi Del</h4>
-                    <div class="table-responsive pt-3">
-                      <table class="table table-bordered">
-                        <thead>
-                          <tr>
-                            <th>#</th>
-                            <th>Nama</th>
-                            <th>NIDN</th>
-                            <th>Aktif Start</th>
-                            <th>Aktif End</th>
-                            <th>Prodi</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>1</td>
-                            <td>Arie Satia Dharma, S.T, M.Kom.</td>
-                            <td>0927028002</td>
-                            <td>2013-09-19</td>
-                            <td>Dosen Non-PNS</td>
-                            <td>S1 Informatika</td>
-                          </tr>
-                          <tr>
-                            <td>2</td>
-                            <td>Dr. Arlinta Christy Barus, ST., M.InfoTech.</td>
-                            <td>0117027901</td>
-                            <td>2002-11-29</td>
-                            <td>Dosen Non-PNS</td>
-                            <td>S1 Informatika</td>
-                          </tr>
-                          <tr>
-                            <td>3</td>
-                            <td>Dr. Johannes Harungguan Sianipar, S.T., M.T.</td>
-                            <td>0116047301</td>
-                            <td>2002-09-01</td>
-                            <td>Dosen Non-PNS</td>
-                            <td>S1 Informatika</td>
-                          </tr>
-                          <tr>
-                            <td>4</td>
-                            <td>Iustisia Natalia Simbolon, S.Kom., M.T.</td>
-                            <td>0114129002</td>
-                            <td>2019-08-05</td>
-                            <td>Dosen Non-PNS</td>
-                            <td>S1 Informatika</td>
-                          </tr>
-                          <tr>
-                            <td>5</td>
-                            <td>Nenni Mona Aruan, S.Pd., M.Si</td>
-                            <td>0128109001</td>
-                            <td>2016-09-01</td>
-                            <td>Dosen Non-PNS</td>
-                            <td>S1 Informatika</td>
-                          </tr>
-                          <tr>
-                            <td>6</td>
-                            <td>Tahan HJ Sihombing, S.Pd., M. App Ling (TESOL)</td>
-                            <td>0102069002</td>
-                            <td>2019-04-23</td>
-                            <td>Dosen Non-PNS</td>
-                            <td>S1 Informatika</td>
-                          </tr>
-                          <tr>
-                            <td>7</td>
-                            <td>Asido Saragih, S.Pd., M.Sc.</td>
-                            <td>0624109301</td>
-                            <td>2022-09-30</td>
-                            <td>Dosen Non-PNS</td>
-                            <td>S1 Informatika</td>
-                          </tr>
-                          <tr>
-                            <td>8</td>
-                            <td>Marojahan MT. Sigiro, ST., M.Sc</td>
-                            <td>0108098301</td>
-                            <td>2007-03-19</td>
-                            <td>Dosen Non-PNS</td>
-                            <td>DIII-Teknologi Komputer</td>
-                          </tr>
-                          <tr>
-                            <td>9</td>
-                            <td>Ardiles Sinaga, S.T., M.T.</td>
-                            <td>0420098501</td>
-                            <td>2022-02-02</td>
-                            <td>Dosen Non-PNS</td>
-                            <td>S1 Informatika</td>
-                          </tr>
-                          <tr>
-                            <td>10</td>
-                            <td>Herimanto, S.Kom., M.Kom</td>
-                            <td>-</td>
-                            <td>2022-08-30</td>
-                            <td>Dosen Non-PNS</td>
-                            <td>S1 Informatika</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-12 grid-margin stretch-card mt-60">
-                <div class="card shadow rounded">
-                  <div class="card-body">
-                    <h4 class="card-title text-center">Matakuliah </h4>
-                    <div class="table-responsive pt-3">
-                      <table class="table table-dark">
-                        <thead>
-                          <tr>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Matakuliah</th>
-                            <th>Bobot SKS</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>1</td>
-                            <td>Arie Satia Dharma, S.T, M.Kom.</td>
-                            <td>11S3109 - Web Based Application Development</td>
-                            <td>4 SKS</td>
-                          </tr>
-                          <tr>
-                            <td>2</td>
-                            <td>Dr. Arlinta Christy Barus, ST., M.InfoTech.</td>
-                            <td>11S3112 - Software Quality Assurance and Testing</td>
-                            <td>3 SKS</td>
-                          </tr>
-                          <tr>
-                            <td>3</td>
-                            <td>Dr. Johannes Harungguan Sianipar, S.T., M.T.</td>
-                            <td>11S3105 - Information Security and Cryptography</td>
-                            <td>3 SKS</td>
-                          </tr>
-                          <tr>
-                            <td>4</td>
-                            <td>Iustisia Natalia Simbolon, S.Kom., M.T.</td>
-                            <td>12S2102 - Database</td>
-                            <td>4 SKS</td>
-                          </tr>
-                          <tr>
-                            <td>5</td>
-                            <td>Nenni Mona Aruan, S.Pd., M.Si</td>
-                            <td>FIS1202 - Physics</td>
-                            <td>3 SKS</td>
-                          </tr>
-                          <tr>
-                            <td>6</td>
-                            <td>Tahan HJ Sihombing, S.Pd., M. App Ling (TESOL)</td>
-                            <td>KUS1201 - English</td>
-                            <td>2 SKS</td>
-                          </tr>
-                          <tr>
-                            <td>7</td>
-                            <td>Asido Saragih, S.Pd., M.Sc.</td>
-                            <td>10S1201 - Basic Programming</td>
-                            <td>4 SKS</td>
-                          </tr>
-                          <tr>
-                            <td>8</td>
-                            <td>Marojahan MT. Sigiro, ST., M.Sc</td>
-                            <td>10S2202 - Computer Networks</td>
-                            <td>3 SKS</td>
-                          </tr>
-                          <tr>
-                            <td>9</td>
-                            <td>Ardiles Sinaga, S.T., M.T.</td>
-                            <td>11S2110 - Object-Oriented Programming</td>
-                            <td>4 SKS</td>
-                          </tr>
-                          <tr>
-                            <td>10</td>
-                            <td>Herimanto, S.Kom., M.Kom</td>
-                            <td>10S3109 - Artificial Intelligence</td>
-                            <td>3 SKS</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-  </template>
+  <div class="pseudoheader p-5 mb-2"></div>
+   <div class="main-panel m-5 p-5">
+           <div class="content-wrapper">
+             <div class="row">
+               <div class="col-lg-12 grid-margin stretch-card">
+                 <div class="card shadow rounded">
+                   <div class="card-body">
+                     <h4 class="card-title text-center">Daftar Dosen Institut Teknologi Del</h4>
+                     <div class="table-responsive pt-3">
+                       <table class="table table-bordered">
+                         <thead>
+                           <tr>
+                             <th>#</th>
+                             <th>Nama</th>
+                             <th>NIDN</th>
+                             <th>Aktif Start</th>
+                             <th>Aktif End</th>
+                             <th>Prodi</th>
+                           </tr>
+                         </thead>
+                         <tbody>
+                           <tr>
+                             <td>1</td>
+                             <td>Arie Satia Dharma, S.T, M.Kom.</td>
+                             <td>0927028002</td>
+                             <td>2013-09-19</td>
+                             <td>Dosen Non-PNS</td>
+                             <td>S1 Informatika</td>
+                           </tr>
+                           <tr>
+                             <td>2</td>
+                             <td>Dr. Arlinta Christy Barus, ST., M.InfoTech.</td>
+                             <td>0117027901</td>
+                             <td>2002-11-29</td>
+                             <td>Dosen Non-PNS</td>
+                             <td>S1 Informatika</td>
+                           </tr>
+                           <tr>
+                             <td>3</td>
+                             <td>Dr. Johannes Harungguan Sianipar, S.T., M.T.</td>
+                             <td>0116047301</td>
+                             <td>2002-09-01</td>
+                             <td>Dosen Non-PNS</td>
+                             <td>S1 Informatika</td>
+                           </tr>
+                           <tr>
+                             <td>4</td>
+                             <td>Iustisia Natalia Simbolon, S.Kom., M.T.</td>
+                             <td>0114129002</td>
+                             <td>2019-08-05</td>
+                             <td>Dosen Non-PNS</td>
+                             <td>S1 Informatika</td>
+                           </tr>
+                           <tr>
+                             <td>5</td>
+                             <td>Nenni Mona Aruan, S.Pd., M.Si</td>
+                             <td>0128109001</td>
+                             <td>2016-09-01</td>
+                             <td>Dosen Non-PNS</td>
+                             <td>S1 Informatika</td>
+                           </tr>
+                           <tr>
+                             <td>6</td>
+                             <td>Tahan HJ Sihombing, S.Pd., M. App Ling (TESOL)</td>
+                             <td>0102069002</td>
+                             <td>2019-04-23</td>
+                             <td>Dosen Non-PNS</td>
+                             <td>S1 Informatika</td>
+                           </tr>
+                           <tr>
+                             <td>7</td>
+                             <td>Asido Saragih, S.Pd., M.Sc.</td>
+                             <td>0624109301</td>
+                             <td>2022-09-30</td>
+                             <td>Dosen Non-PNS</td>
+                             <td>S1 Informatika</td>
+                           </tr>
+                           <tr>
+                             <td>8</td>
+                             <td>Marojahan MT. Sigiro, ST., M.Sc</td>
+                             <td>0108098301</td>
+                             <td>2007-03-19</td>
+                             <td>Dosen Non-PNS</td>
+                             <td>DIII-Teknologi Komputer</td>
+                           </tr>
+                           <tr>
+                             <td>9</td>
+                             <td>Ardiles Sinaga, S.T., M.T.</td>
+                             <td>0420098501</td>
+                             <td>2022-02-02</td>
+                             <td>Dosen Non-PNS</td>
+                             <td>S1 Informatika</td>
+                           </tr>
+                           <tr>
+                             <td>10</td>
+                             <td>Herimanto, S.Kom., M.Kom</td>
+                             <td>-</td>
+                             <td>2022-08-30</td>
+                             <td>Dosen Non-PNS</td>
+                             <td>S1 Informatika</td>
+                           </tr>
+                         </tbody>
+                       </table>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+               <div class="col-lg-12 grid-margin stretch-card mt-60">
+                 <div class="card shadow rounded">
+                   <div class="card-body">
+                     <h4 class="card-title text-center">Matakuliah </h4>
+                     <div class="table-responsive pt-3">
+                       <table class="table table-dark">
+                         <thead>
+                           <tr>
+                             <th>#</th>
+                             <th>Name</th>
+                             <th>Matakuliah</th>
+                             <th>Bobot SKS</th>
+                           </tr>
+                         </thead>
+                         <tbody>
+                           <tr>
+                             <td>1</td>
+                             <td>Arie Satia Dharma, S.T, M.Kom.</td>
+                             <td>11S3109 - Web Based Application Development</td>
+                             <td>4 SKS</td>
+                           </tr>
+                           <tr>
+                             <td>2</td>
+                             <td>Dr. Arlinta Christy Barus, ST., M.InfoTech.</td>
+                             <td>11S3112 - Software Quality Assurance and Testing</td>
+                             <td>3 SKS</td>
+                           </tr>
+                           <tr>
+                             <td>3</td>
+                             <td>Dr. Johannes Harungguan Sianipar, S.T., M.T.</td>
+                             <td>11S3105 - Information Security and Cryptography</td>
+                             <td>3 SKS</td>
+                           </tr>
+                           <tr>
+                             <td>4</td>
+                             <td>Iustisia Natalia Simbolon, S.Kom., M.T.</td>
+                             <td>12S2102 - Database</td>
+                             <td>4 SKS</td>
+                           </tr>
+                           <tr>
+                             <td>5</td>
+                             <td>Nenni Mona Aruan, S.Pd., M.Si</td>
+                             <td>FIS1202 - Physics</td>
+                             <td>3 SKS</td>
+                           </tr>
+                           <tr>
+                             <td>6</td>
+                             <td>Tahan HJ Sihombing, S.Pd., M. App Ling (TESOL)</td>
+                             <td>KUS1201 - English</td>
+                             <td>2 SKS</td>
+                           </tr>
+                           <tr>
+                             <td>7</td>
+                             <td>Asido Saragih, S.Pd., M.Sc.</td>
+                             <td>10S1201 - Basic Programming</td>
+                             <td>4 SKS</td>
+                           </tr>
+                           <tr>
+                             <td>8</td>
+                             <td>Marojahan MT. Sigiro, ST., M.Sc</td>
+                             <td>10S2202 - Computer Networks</td>
+                             <td>3 SKS</td>
+                           </tr>
+                           <tr>
+                             <td>9</td>
+                             <td>Ardiles Sinaga, S.T., M.T.</td>
+                             <td>11S2110 - Object-Oriented Programming</td>
+                             <td>4 SKS</td>
+                           </tr>
+                           <tr>
+                             <td>10</td>
+                             <td>Herimanto, S.Kom., M.Kom</td>
+                             <td>10S3109 - Artificial Intelligence</td>
+                             <td>3 SKS</td>
+                           </tr>
+                         </tbody>
+                       </table>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+   </template>
